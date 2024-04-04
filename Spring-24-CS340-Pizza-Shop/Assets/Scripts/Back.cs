e5f61a7188e9b199001fd2ffbf4ef8c09bfcc5a1
Ferrochrom3 <132790622+Ferrochrom3@users.noreply.github.com>
2024-04-04 14:27:51 -0400

    Json file
    Added manager and employee login credentials to a json file

--- Spring-24-CS340-Pizza-Shop/Assets/Scripts/Back.cs
+++ Spring-24-CS340-Pizza-Shop/Assets/Scripts/Back.cs
@@ -1,14 +1,41 @@
-using UnityEngine;
-
+/*using UnityEngine;
+using System.Collections.Generic;
 // to test the back button, only start at the login page
 public class Back : MonoBehaviour
+    private List<string> PreviousScene = new List<string>();
+private void Start()
+{
+    Back.Add(Application.loadedLevelName);
+    GameObject.SetGameObjectsActive(false);
+
+}
+
+public void AddCurrentSceneToLoadedScenes()
+{
+    PreviousScene.Add(Application.loadedLevelName);
+
+}
+
+public void LoadPreviousScene()
+{
+    string previousScene = string.Empty;
+    if (previousScene.Count > 1) {
+    previousScene = [previousScene.Count -1]};
+    previousScene.RemoveAt(previousScene.Count - 1);
+    Application.LoadLevel(previousScene);
+}
+else
+{ PreviousScene = PreviousScene[0]
+        Application.LoadLevel(previousScene)
+        gameObject.SetActive(false);
+}
 {
     //List<String> BackBM = new List <String>();
 
-    public void goBack()
-    {
-        Debug.Log("The method worked yippee");
+   // public void goBack()
+   // {
+     ///   Debug.Log("The method worked yippee");
 
 
-    }
-}
+   // }
+}*/
